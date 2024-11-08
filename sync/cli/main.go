c35180d750495c4c8e69a31af85c5dca0c08d317
Daniel <12280220+bredtape@users.noreply.github.com>
2024-11-08 15:19:06 +0100

    cli, add sync ls and info

--- sync/cli/main.go
+++ sync/cli/main.go
@@ -25,7 +25,10 @@ command tree:
 	sync
 	  start [from] [to] [source stream name] # start sync with deliver policy all, no subject filters
 		stop  [from] [to] [source stream name] # stop sync
+		ls [from] [to] # list source stream names being sync'ed
+		info [from] [to] [source stream name] # get sync info
 
+NOT IMPLEMENTED:
 	stream
 	  info [stream name] # get stream info
 */
@@ -151,6 +154,64 @@ func main() {
 							return syncStop(c.Context, js, syncStreamName, from, to, streamName)
 						},
 					},
+					{
+						Name:  "ls",
+						Usage: "ls [from] [to]",
+						Action: func(c *cli.Context) error {
+							js, err := getJS(c)
+							if err != nil {
+								return err
+							}
+
+							syncStreamName, err := getSyncStreamName(c)
+							if err != nil {
+								return err
+							}
+
+							from := c.Args().Get(0)
+							to := c.Args().Get(1)
+							if from == "" || to == "" {
+								return errors.New("from and to are required")
+							}
+
+							xs, err := syncList(c.Context, js, syncStreamName, from, to)
+							if err != nil {
+								return err
+							}
+
+							fmt.Fprintf(os.Stdout, "%v\n", xs)
+							return nil
+						},
+					},
+					{
+						Name:  "info",
+						Usage: "info [from] [to] [sourceStreamName]",
+						Action: func(c *cli.Context) error {
+							js, err := getJS(c)
+							if err != nil {
+								return err
+							}
+
+							syncStreamName, err := getSyncStreamName(c)
+							if err != nil {
+								return err
+							}
+
+							from := c.Args().Get(0)
+							to := c.Args().Get(1)
+							streamName := c.Args().Get(2)
+							if from == "" || to == "" || streamName == "" {
+								return errors.New("from, to and stream name are required")
+							}
+							xs, err := syncInfo(c.Context, js, syncStreamName, from, to, streamName)
+							if err != nil {
+								return err
+							}
+
+							fmt.Fprintf(os.Stdout, "%v\n", xs)
+							return nil
+						},
+					},
 				},
 			},
 		}}
@@ -209,6 +270,66 @@ func syncStop(ctx context.Context, js *sync.JSConn, syncStreamName, from, to, st
 	subject := fmt.Sprintf("%s.%s.%s.%s", syncStreamName, to, from, streamName)
 	_, err := js.PublishProto(ctx, subject, nil, req, jetstream.WithExpectStream(syncStreamName))
 	return err
+}
+
+func syncList(ctx context.Context, js *sync.JSConn, syncStreamName, from, to string) ([]string, error) {
+	head, err := js.GetLastSequence(ctx, syncStreamName)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get last sequence")
+	}
+
+	var xs []string
+	ch := js.StartSubscribeOrderered(ctx, syncStreamName, jetstream.OrderedConsumerConfig{DeliverPolicy: jetstream.DeliverLastPerSubjectPolicy})
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case msg, ok := <-ch:
+			if !ok {
+				return xs, errors.New("did not receive all messages")
+			}
+			if strings.Contains(msg.Subject, fmt.Sprintf(".%s.%s.", to, from)) {
+				xs = append(xs, msg.Subject)
+			}
+
+			if msg.Sequence == head {
+				return xs, nil
+			}
+		}
+	}
+}
+
+func syncInfo(ctx context.Context, js *sync.JSConn, syncStreamName, from, to, sourceStreamName string) ([]*v1.StartSyncRequest, error) {
+	head, err := js.GetLastSequence(ctx, syncStreamName)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get last sequence")
+	}
+
+	var xs []*v1.StartSyncRequest
+	ch := js.StartSubscribeOrderered(ctx, syncStreamName, jetstream.OrderedConsumerConfig{DeliverPolicy: jetstream.DeliverLastPerSubjectPolicy})
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case msg, ok := <-ch:
+			if !ok {
+				return xs, errors.New("did not receive all messages")
+			}
+			if strings.Contains(msg.Subject, fmt.Sprintf(".%s.%s.%s", to, from, sourceStreamName)) {
+				var x v1.StartSyncRequest
+				err = proto.Unmarshal(msg.Data, &x)
+				if err != nil {
+					return nil, errors.Wrap(err, "failed to unmarshal message")
+				}
+
+				xs = append(xs, &x)
+			}
+
+			if msg.Sequence == head {
+				return xs, nil
+			}
+		}
+	}
 }
 
 func getJS(c *cli.Context) (*sync.JSConn, error) {
